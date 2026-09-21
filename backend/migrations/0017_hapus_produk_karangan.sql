17867745f23a95c26f3305745a128e89d22ee618
winterheatherica <winterheatherica@gmail.com>
2026-09-21 21:58:31 +0700

    delete six fabricated products that never existed on shopee

---
+++ backend/migrations/0017_hapus_produk_karangan.sql
@@ -0,0 +1,9 @@
+DELETE FROM products
+WHERE slug IN (
+  'maggpro-cat-food-supplement',
+  'maggpro-dog-food-supplement',
+  'maggpro-omega-oil',
+  'proteinmagg-supplement',
+  'sangreat-bag-cleaner-praktis',
+  'sangreat-bag-deodorizer'
+);
